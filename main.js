e1d8107ee453242ca323347e1032122a041cecc9
Kilian Sicot <sicot_k@etna-alternance.net>
2022-05-06 12:44:20 +0200

    ajout js

---
+++ main.js
@@ -0,0 +1,17 @@
+let  mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+const linkWork = document.querySelectorAll('.work__item')
+
+function activeWork() {
+    linkWork.forEach(L=> l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+LinkWork.forEach(L=> l.addEventListener('click', activeWork))
